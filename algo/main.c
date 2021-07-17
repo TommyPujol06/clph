199c9cda427442e0606bd7ad55d78331f1e450b9
Tommy Pujol <git@itstommy.xyz>
2021-07-17 14:16:51 +0200

    Finish up tests for algo/main.c

--- algo/main.c
+++ algo/main.c
@@ -1,6 +1,12 @@
 #include <stdint.h>
 #include <stdbool.h>
+
+#define __TESTS_ENABLED__
+
+#ifdef __TESTS_ENABLED__
+#include <stdlib.h>
 #include <stdio.h>
+#endif
 
 inline __attribute__((always_inline)) uint16_t
 pixel_value(uint8_t r, uint8_t g, uint8_t b)
@@ -9,20 +15,20 @@ pixel_value(uint8_t r, uint8_t g, uint8_t b)
 	return r + g + b;
 }
 
-inline __attribute__((always_inline)) uint32_t
-chunk_value(uint16_t pixel, uint32_t * chunk)
+inline __attribute__((always_inline)) uint16_t
+chunk_value(uint16_t pixel, uint16_t * chunk)
 {
 	*chunk += pixel;
 }
 
-uint16_t
-mean(uint32_t total, uint32_t size)
+inline __attribute__((always_inline)) uint16_t
+mean(uint16_t total, uint16_t size)
 {
-	return (uint16_t)(total / size);
+	return total / size;
 }
 
 uint16_t
-get_max_diff(uint16_t last, uint16_t first, uint16_t size)
+get_max_diff(uint16_t first, uint16_t last, uint16_t size)
 {
 	return (uint16_t)(((last - first) / (size - 1)) / 2);
 }
@@ -31,6 +37,14 @@ inline __attribute__((always_inline)) bool
 is_similar(uint16_t c1, uint16_t c2, uint16_t max_diff)
 {
 	return c1 - c2 <= max_diff;
+}
+
+#ifdef __TESTS_ENABLED__
+void
+fail(const char * err)
+{
+	fprintf(stderr, err);
+	exit(1);
 }
 
 int
@@ -43,32 +57,42 @@ test(void)
 		pixels[i] = pixel_value(i, i, i);
 
 	if (pixels[0] != 0 || pixels[255] != 765)
-		return -1;
+		fail("Did not initialize pixels properly.\n");
 
 	uint16_t idx = 0;
-	uint32_t chunk = 0;
-	for (uint16_t j=0; j != 256; j++) {
-		if (j % 4 == 0) {
+	uint16_t chunk = 0;
+	for (uint16_t i=1; i != 257; i++) {
+		if (i % 4 == 0) {
 			chunks[idx] = mean(chunk, 4);
 			chunk = 0;
 			idx++;
 		}
 
-		chunk_value(pixels[j], &chunk);
+		chunk_value(pixels[i], &chunk);
 	}
 
-	chunks[++idx] = mean(chunk, 4);
-	chunk = 0;
-
-	printf("%d\n%d\n", chunks[0], chunks[63]);
 	if (chunks[0] != 4 || chunks[63] != 760)
-		return -1;
+		fail("chunks were not properly calculated.\n");
 
+
+	uint16_t max_diff = get_max_diff(chunks[0], chunks[63], 64);
+	if (max_diff != 6)
+		fail("max difference was not properly calculated.\n");
+
+	if (is_similar(chunks[0], chunks[1], max_diff) != true)
+		fail("c0 and c1 should be similar!\n");
+
+	fprintf(stdout, "All tests successfully passed!\n");
 	return 0;
 }
+#endif
 
 int
 main(void)
 {
+#ifdef __TESTS_ENABLED__
 	return test();
+#else
+	return 0;
+#endif
 }
